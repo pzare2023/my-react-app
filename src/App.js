1bf0e53e82600f4b10b8c79637bfb2d946bfd36e
pzare2023 <zzz.parnia@gmail.com>
2024-12-04 14:43:31 -0500

    Initial commit for React app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Web Page</h1>
+      <p>This page was built with React!</p>
+      <h2>Steps to Install React:</h2>
+      <ol>
+        <li>Install Node.js and npm.</li>
+        <li>Run `npx create-react-app my-react-app`.</li>
+      </ol>
+      <h2>Difficulties Encountered:</h2>
+      <p>One challenge was installing Node.js, which I resolved by...</p>
     </div>
   );
 }
